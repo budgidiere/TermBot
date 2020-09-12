d8b57481fe0a6a5f48e4ab2155f455f1dc609ed6
Bud Gidiere <sgidiere@gmail.com>
2020-09-12 18:49:52 -0500

    await all returns

--- database/cache.py
+++ database/cache.py
@@ -26,7 +26,7 @@ from aiofile import *
 async def retrieve_data(file):
     try:
         if asyncio.run(init_cache_file(file)) == False:
-            return False
+            return await False
         async with AIOFile("cache/" + file + ".p", 'wb') as afp:
             data = dill.loads(asyncio.run(afp.read()))
 
@@ -34,9 +34,9 @@ async def retrieve_data(file):
             return data["data"]
         del data
         asyncio.run(purge_data(file))
-        return False
+        return await False
     except:
-        return False
+        return await False
 
 
 async def store_data(file, data, stale):
@@ -46,36 +46,36 @@ async def store_data(file, data, stale):
             cache["data"] = data
             cache["expiretime"] = (time.time() + float(stale))
             await afp.write(dill.dumps(cache))
-            return True
+            return await True
     except:
-        return False
+        return await False
 
 
 async def purge_data(file):
     try:
         assert AIOFile.os.remove("cache/" + file + ".p")
-        return True
+        return await True
     except:
-        return False
+        return await False
 
 
 async def retrieve_data_rolling(key, stale):
     data = retrieve_data(key)
     if data != False:
         await store_data(key, data, stale)
-    return data
+    return await data
 
 
 async def pass_and_store(file, data, stale):
     asyncio.run(store_data(file, data, stale))
-    return data
+    return await data
 
 
 async def init_cache_file(file):
     try:
         if os.exists("cache/" + file + ".p"):
-            return True
+            return await True
         assert AIOFile.os.stat("cache/" + file + ".p").st_size > 0
-        return False
+        return await False
     except:
-        return False
+        return await False
