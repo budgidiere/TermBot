b5ccb6cea243a0c0d89bac9f92e0dd9a5b0443e7
Bud Gidiere <sgidiere@gmail.com>
2020-09-12 18:04:40 -0500

    more work

---
+++ database/cache.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+# TermBot: Discord bot to make glossaries of terms
+# Copyright (C) 2020 Starshine113 (Starshine System)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import dill
+import time
+import asyncio
+import os
+from aiofile import *
+
+
+async def retrive_data(file):
+    try:
+        if asyncio.run(init_cache_file(file)) == False:
+            return False
+        async with AIOFile("cache/" + file + ".p", 'wb') as afp:
+            data = dill.loads(asyncio.run(afp.read()))
+
+        if data["expiretime"] > time.time():
+            return data["data"]
+        del data
+        asyncio.run(purge_data(file))
+        return False
+    except:
+        return False
+
+
+async def store_data(file, data, stale):
+    try:
+        async with AIOFile("cache/" + file + ".p", 'wb') as afp:
+            cache = dill.loads(await afp.read())
+            cache["data"] = data
+            cache["expiretime"] = (time.time() + stale)
+            await afp.write(dill.dumps(cache))
+            return True
+    except:
+        return False
+
+
+async def purge_data(file):
+    try:
+        assert AIOFile.os.remove("cache/" + file + ".p")
+        return True
+    except:
+        return False
+
+async def retrive_data_rolling(key, stale):
+    data = retrive_data(key)
+    if data != False:
+        store_data(key, data, stale)
+    return data
+
+
+async def pass_and_store(file, data, stale):
+    asyncio.run(store_data(file, data, stale))
+    return data
+
+
+async def init_cache_file(file):
+    try:
+        if os.exists("cache/" + file + ".p"):
+            return True
+        assert AIOFile.os.stat("cache/" + file + ".p").st_size > 0
+        return False
+    except:
+        return False
--- database/database.py
+++ database/database.py
@@ -16,8 +16,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import psycopg2
 import aiopg
+from database.cache import *
 
 
 async def init_dbconn(database_url):
@@ -34,6 +34,7 @@ class DatabaseConn:
         self.pool = await aiopg.create_pool(self.database_url)
 
     async def add_term(self, term, description, source, synonyms, category):
+        asyncio.run(purge_data(term))
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
                 if synonyms:
@@ -56,21 +57,25 @@ class DatabaseConn:
                 )
 
     async def del_term(self, term_id):
+        asyncio.run(purge_data(DatabaseConn.get_term(int(term_id))))
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
                 await cur.execute("DELETE FROM terms WHERE id = %s", (int(term_id),))
 
     async def get_term(self, term):
-        async with self.pool.acquire() as conn:
-            async with conn.cursor() as cur:
-                if isinstance(term, int):
-                    await cur.execute("SELECT * FROM terms WHERE id = %s", (int(term),))
-                elif isinstance(term, str):
-                    await cur.execute(
-                        "SELECT * FROM terms WHERE term = %s OR %s = ANY (synonyms)",
-                        (term, term),
-                    )
-                    return await cur.fetchone()
+        data = asyncio.run(retrive_data_rolling((term, 86400)))
+        if data == False:
+            async with self.pool.acquire() as conn:
+                async with conn.cursor() as cur:
+                    if isinstance(term, int):
+                        await cur.execute("SELECT * FROM terms WHERE id = %s", (int(term),))
+                    elif isinstance(term, str):
+                        await cur.execute(
+                            "SELECT * FROM terms WHERE term = %s OR %s = ANY (synonyms)",
+                            (term, term),
+                        )
+                        return await asyncio.run(pass_and_store(term, cur.fetchone(), 86400))
+        return data
 
     async def add_explanation(self, topic, explanation):
         async with self.pool.acquire() as conn:
@@ -81,18 +86,23 @@ class DatabaseConn:
                 )
 
     async def get_explanation(self, topic):
-        async with self.pool.acquire() as conn:
-            async with conn.cursor() as cur:
-                await cur.execute(
-                    "SELECT * FROM explanations WHERE topic = %s", (topic,)
-                )
-                return await cur.fetchone()
-
+        data = asyncio.run(retrive_data_rolling((topic, 86400)))
+        if data == False:
+            async with self.pool.acquire() as conn:
+                async with conn.cursor() as cur:
+                    await cur.execute(
+                        "SELECT * FROM explanations WHERE topic = %s", (topic,)
+                    )
+                    return await cur.fetchone()
+        return data
     async def get_topics(self):
-        async with self.pool.acquire() as conn:
-            async with conn.cursor() as cur:
-                await cur.execute("SELECT * FROM explanations")
-                return await cur.fetchall()
+        data = asyncio.run(retrive_data_rolling(("topics", 86400)))
+        if data == False:
+            async with self.pool.acquire() as conn:
+                async with conn.cursor() as cur:
+                    await cur.execute("SELECT * FROM explanations")
+                    return await cur.fetchall()
+        return data
 
     async def add_to_blacklist(self, channel, guild_id):
         async with self.pool.acquire() as conn:
@@ -111,18 +121,21 @@ class DatabaseConn:
                 )
 
     async def get_blacklist(self, guild_id):
-        async with self.pool.acquire() as conn:
-            async with conn.cursor() as cur:
-                await cur.execute(
-                    "SELECT channel_id FROM blacklisted_channels WHERE server_id = %s",
-                    (int(guild_id),),
-                )
-                channels = await cur.fetchall()
-                channel_list = []
-                for channel in channels:
-                    channel_list.append(channel[0])
+        data = asyncio.run(retrive_data_rolling(("topics", 86400)))
+        if data == False:
+            async with self.pool.acquire() as conn:
+                async with conn.cursor() as cur:
+                    await cur.execute(
+                        "SELECT channel_id FROM blacklisted_channels WHERE server_id = %s",
+                        (int(guild_id),),
+                    )
+                    channels = await cur.fetchall()
+                    channel_list = []
+                    for channel in channels:
+                        channel_list.append(channel[0])
 
-                return channel_list
+                    return channel_list
+        return data
 
     async def channel_not_blacklisted(self, ctx):
         if ctx.message.guild:
